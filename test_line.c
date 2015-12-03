6a40aa2f3767265ec039b1285ea9b8f99cbf85d2
LeGuit <guillaume.woodward@gmail.com>
2015-12-03 20:54:14 +0100

    Test was good file_test was wrong

--- test_line.c
+++ test_line.c
@@ -29,7 +29,7 @@ static int		test_input(char *buf)
 	return (1);
 }
 */
-/*
+
 static int		test_4tetri(char *buf)
 {
 	int			i;
@@ -48,36 +48,29 @@ static int		test_4tetri(char *buf)
 		return (0);
 	return (1);
 }
-*/
+
 static int		test_shape(char *buf)
 {
 	int			i;
-	int			count;
 
 	i = 0;
-	count = 0;
-	while (i < 21)
+	while (i < 16)
 	{
-		if (buf[i] == '#')
-		{
-			count++;		//Nawak !!!!!
-			if (count == 4)
-				return (1);
-		ft_putstr("count : ");
-		ft_putnbr(count);
-		ft_putchar('\n');
-		ft_putstr("buf : ");
-		ft_putchar(buf[i]);
-		ft_putchar('\n');
-		ft_putstr("buf + 1 : ");
-		ft_putchar(buf[i + 1]);
-		ft_putchar('\n');
-		ft_putstr("buf + 5 : ");
-		ft_putchar(buf[i + 5]);
-		ft_putchar('\n');
-			if (buf[i + 1] == '#' || buf[i + 5] == '#')
-				continue ;
-		}
+		if (buf[i] == '#'
+			&& buf[i + 1] == '#' && buf[i + 5] == '#')
+		return (0);
+//		ft_putstr("count : ");
+//		ft_putnbr(count);
+//		ft_putchar('\n');
+//		ft_putstr("buf : ");
+//		ft_putchar(buf[i]);
+//		ft_putchar('\n');
+//		ft_putstr("buf + 1 : ");
+//		ft_putchar(buf[i + 1]);
+//		ft_putchar('\n');
+//		ft_putstr("buf + 5 : ");
+//		ft_putchar(buf[i + 5]);
+//		ft_putchar('\n');
 		i++;
 	}
 	return (0);
@@ -85,8 +78,8 @@ static int		test_shape(char *buf)
 
 int				is_valid(char *buf)
 {
-	if (/*test_4tetri(buf)
-		  && test_input(buf)
+	if (test_4tetri(buf)
+		  && /*test_input(buf)
 		  &&*/ test_shape(buf))
 		return (1);
 	return (0);
