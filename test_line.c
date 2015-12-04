e4a10a6a6d4cd80c33cb9fd336d8a641bb779447
Guillaume WOODWARD <gwoodwar@e1r5p21.42.fr>
2015-12-04 11:27:01 +0100

    test endl

--- test_line.c
+++ test_line.c
@@ -6,30 +6,29 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/03 15:00:15 by gwoodwar          #+#    #+#             */
-/*   Updated: 2015/12/03 20:11:05 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2015/12/04 11:26:32 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-/*
-static int		test_input(char *buf)
+
+static int		test_endl(char *buf)
 {
 	int			i;
 
 	i = 0;
-	while (i < 21)
+	while (i < 20)
 	{
-		if ((i % 5 == 4 && buf[i] != '\n')
-				|| (i % 5 != 4 && (buf[i] != '#' || buf[i] != '.')))
+		if (i % 5 == 4 && buf[i] != '\n')
 			return (0);
 		i++;
 	}
-	if (buf[i] != '\n')
+	if (ft_strlen (buf) == 21 && buf[i] != '\n')
 		return (0);
 	return (1);
 }
-*/
 
+/*
 static int		test_4tetri(char *buf)
 {
 	int			i;
@@ -53,34 +52,25 @@ static int		test_shape(char *buf)
 {
 	int			i;
 
-	i = 0;
+	i = 5;
 	while (i < 16)
 	{
-		if (buf[i] == '#'
-			&& buf[i + 1] == '#' && buf[i + 5] == '#')
+		if (buf[i] == '#')
+		{
+			if (buf[i - 1] != '#' && buf[i + 1] != '#'
+					&& buf[i + 5] != '#' && buf[i - 5] != '#')
 		return (0);
-//		ft_putstr("count : ");
-//		ft_putnbr(count);
-//		ft_putchar('\n');
-//		ft_putstr("buf : ");
-//		ft_putchar(buf[i]);
-//		ft_putchar('\n');
-//		ft_putstr("buf + 1 : ");
-//		ft_putchar(buf[i + 1]);
-//		ft_putchar('\n');
-//		ft_putstr("buf + 5 : ");
-//		ft_putchar(buf[i + 5]);
-//		ft_putchar('\n');
+		}
 		i++;
 	}
-	return (0);
+	return (1);
 }
-
+*/
 int				is_valid(char *buf)
 {
-	if (test_4tetri(buf)
-		  && /*test_input(buf)
-		  &&*/ test_shape(buf))
+	if (/*test_4tetri(buf)
+		  &&*/ test_endl(buf)
+		 /* && test_shape(buf)*/)
 		return (1);
 	return (0);
 }
