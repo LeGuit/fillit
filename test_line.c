bddf9facb85ca190b42bf3e5e090e83f62541416
Guillaume WOODWARD <gwoodwar@e1r5p21.42.fr>
2015-12-04 11:46:17 +0100

    test_line ok

--- test_line.c
+++ test_line.c
@@ -6,7 +6,7 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/03 15:00:15 by gwoodwar          #+#    #+#             */
-/*   Updated: 2015/12/04 11:26:32 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2015/12/04 11:45:50 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,11 @@ static int		test_endl(char *buf)
 			return (0);
 		i++;
 	}
-	if (ft_strlen (buf) == 21 && buf[i] != '\n')
+	if (ft_strlen(buf) == 21 && buf[i] != '\n')
 		return (0);
 	return (1);
 }
 
-/*
 static int		test_4tetri(char *buf)
 {
 	int			i;
@@ -59,18 +58,34 @@ static int		test_shape(char *buf)
 		{
 			if (buf[i - 1] != '#' && buf[i + 1] != '#'
 					&& buf[i + 5] != '#' && buf[i - 5] != '#')
-		return (0);
+				return (0);
 		}
 		i++;
 	}
 	return (1);
 }
-*/
+
+static int		test_char(char *buf)
+{
+	int			i;
+
+	i = 0;
+	while (i < 19)
+	{
+		if (i % 5 != 4 && buf[i] != '#' && buf[i] != '.')
+			return (0);
+		i++;
+	}
+	return (1);
+
+}
+
 int				is_valid(char *buf)
 {
-	if (/*test_4tetri(buf)
-		  &&*/ test_endl(buf)
-		 /* && test_shape(buf)*/)
+	if (test_4tetri(buf)
+			&& test_endl(buf)
+			&& test_shape(buf)
+			&& test_char(buf))
 		return (1);
 	return (0);
 }
